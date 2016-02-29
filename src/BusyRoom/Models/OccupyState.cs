a4b5bc7f2932b07fbd2c471178a810988f6af422
Janusz Sołtysiak <jsoltysiak@gmail.com>
2016-02-29 09:31:19 +0100

    Added first version of db models for room and occupy state

---
+++ src/BusyRoom/Models/OccupyState.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BusyRoom.Models
+{
+    public class OccupyState
+    {
+        public int Id { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public bool IsBusy { get; set; }
+
+        public Room Room { get; set; }
+    }
+}
