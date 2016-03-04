d4fdab9bc57d76d762e723ea3c912305f2261a09
Janusz Sołtysiak <jsoltysiak@gmail.com>
2016-03-04 06:54:08 +0100

    Added RoomController API with initial Get method

---
+++ src/BusyRoom/Controllers/Api/RoomsController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BusyRoom.Models;
+using Microsoft.AspNet.Mvc;
+
+// For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BusyRoom.Controllers.Api
+{
+    [Route("api/[controller]")]
+    public class RoomsController : Controller
+    {
+        private IBusyRoomRepository _repository;
+
+        public RoomsController(IBusyRoomRepository repository)
+        {
+            _repository = repository;
+        }
+
+        // GET: api/rooms
+        [HttpGet]
+        public JsonResult Get()
+        {
+            var rooms = _repository.GetAllRoomsWithOcupyStates();
+            var json = Json(rooms);
+            return json;
+        }
+    }
+}
