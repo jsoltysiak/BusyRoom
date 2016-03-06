22115e0373d72b4547f97c5049ed0eec42449d58
Janusz Sołtysiak <jsoltysiak@gmail.com>
2016-03-06 16:43:52 +0100

    Added RoomsController default POST method

--- src/BusyRoom/Controllers/Api/RoomsController.cs
+++ src/BusyRoom/Controllers/Api/RoomsController.cs
@@ -1,8 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Linq;
+using System.Net;
 using BusyRoom.Models;
+using BusyRoom.ViewModels;
 using Microsoft.AspNet.Mvc;
 
 // For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
@@ -26,6 +25,19 @@ namespace BusyRoom.Controllers.Api
             var rooms = _repository.GetAllRoomsWithOcupyStates();
             var json = Json(rooms);
             return json;
+        }
+
+        [HttpPost]
+        public JsonResult Post([FromBody] RoomViewModel room)
+        {
+            if (ModelState.IsValid)
+            {
+                Response.StatusCode = (int) HttpStatusCode.Created;
+                return Json(room);
+            }
+
+            Response.StatusCode = (int) HttpStatusCode.BadRequest;
+            return Json(ModelState.Values.SelectMany(v => v.Errors).Select(e => e.ErrorMessage));
         }
     }
 }
