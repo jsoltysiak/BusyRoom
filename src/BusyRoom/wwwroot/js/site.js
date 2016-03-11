20183734fac8a2073ed96c921665f1741fa2fa8c
Janusz Sołtysiak <jsoltysiak@gmail.com>
2016-03-11 23:23:20 +0100

    Added first version of chart

--- src/BusyRoom/wwwroot/js/site.js
+++ src/BusyRoom/wwwroot/js/site.js
@@ -1,1 +1,51 @@
 ﻿// Write your Javascript code.
+
+
+//var options = {
+//    height: 200
+//};
+
+
+var now = new Date().getTime();
+
+var states = [];
+var dates = [];
+var occupied = [];
+
+var chartOptions = {
+    low: 0,
+    high: 2,
+    showArea: true,
+    axisY: {
+        onlyInteger: true,
+    },
+    lineSmooth: Chartist.Interpolation.step(),
+    showPoint: false
+}
+
+// Assuming you have referenced jQuery
+$(function() {
+    $.getJSON("/api/rooms/Arduino/states", function(response) {
+        states = response;
+
+        dates = $.map(states, function (el) {
+            var date = new Date(el.createdOn).getTime() - now;
+            return Math.round(date / 60000);
+            
+        });
+
+        occupied = $.map(states, function (el) {
+            return el.isOccupied ? 1 : 0;
+        });
+
+        var data = {
+            labels: dates,
+            series: [
+                occupied
+            ]
+    };
+
+        new Chartist.Line('#chart1', data, chartOptions);
+    });
+
+});
